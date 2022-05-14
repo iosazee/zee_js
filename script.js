88e43cae13b81acd6de652a25d824500de4ea1ee
Zee Mudia <mudiasam42@gmail.com>
2022-05-14 18:49:23 +0100

    built a conditional for an algorthmic stable coin system

--- script.js
+++ script.js
@@ -27,8 +27,26 @@
         console.log("you are not invited to the party")
     }
     else {
-        console.log("you are not attending the party and not old enough")
+      //  console.log("you are not attending the party and not old enough")
     }
+    
+// create an algorithmic stable coin system
+// create two variables stableCoin and volatileCoin
+// if stableCoin < 1, print more VolatileCoin
+// if stableCoin > 1, burn more VolatileCoin
+
+var stableCoin = 1;
+var volatileCoin = 20;
+
+if (stableCoin < 1 && volatileCoin === 20) {
+    console.log("print more volatileCoin")
+}
+else if (stableCoin > 1 && volatileCoin === 20) {
+    console.log("burn more volatileCoin")
+}
+else  {
+    console.log("everything is working fine")
+}
 
 // Use || as well as &&
 // more than, less than operators for numbers
