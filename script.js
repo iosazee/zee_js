b21fd26951503320f9ef5f44b7188d1593769b3e
Zee Mudia <mudiasam42@gmail.com>
2022-05-14 16:56:47 +0100

    built a conditionaal for adult attending a party

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 //Topics to cover
 //Introduction to Conditionals and Pseudocode
+
+    const bananasCount = 5;
+    const applesCount = 10;
+    if (bananasCount > applesCount) {
+     //   console.log("we have more apples than bananas")
+    }
+    else {
+    //    console.log("we have more bananas than apples")
+    }
+
+    // create a conditional that checks if a person is an adult and allowed to attend a party
+    // create two variables isAdult and isAttendingParty
+    // if both are true welcome them to the party
+    // if they are not adult, print they are not old enough
+
+    let isAdult = false;
+    let isAttendingParty = false;
+
+    if (isAdult && isAttendingParty) {
+        console.log("welcome to the party!")
+    }
+    else if (isAdult === false && isAttendingParty === true)
+        console.log("you are not old enough")
+    else if (isAttendingParty === false && isAdult ===true) {
+        console.log("you are not invited to the party")
+    }
+    else {
+        console.log("you are not attending the party and not old enough")
+    }
+
 // Use || as well as &&
 // more than, less than operators for numbers
 // boolean conditionals
