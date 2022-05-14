f9f4bb25a286525296be2a19260f01b9dad4f1e3
Zee Mudia <mudiasam42@gmail.com>
2022-05-14 03:36:36 +0100

    revision of this week js lesson

---
+++ script.js
@@ -0,0 +1,7 @@
+//Topics to cover
+//Introduction to Conditionals and Pseudocode
+// Use || as well as &&
+// more than, less than operators for numbers
+// boolean conditionals
+// ternary operators
+// switch statements
